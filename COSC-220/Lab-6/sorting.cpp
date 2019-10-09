90883f8ad306c2875fa945bd526f54b16b21957c
Blaine-Mason <bmason3@gulls.salisbury.edu>
2019-10-08 23:25:39 -0400

    Code Finished

--- COSC-220/Lab-6/sorting.cpp
+++ COSC-220/Lab-6/sorting.cpp
@@ -2,41 +2,42 @@
 #include <stdio.h>
 #include <chrono>
 #include <time.h>
-void Swap(int& a, int& b){
+void Swap(int& a, int& b, int& c){
+  c++; //haha get it
   int temp = a;
   a = b;
   b = temp;
 }
-void fillArray(aint* arr, int length){
+void fillArray(int* arr, int length){
   srand(time(NULL));
   int n = 100;
   for(int i = 0; i < length; i++){
     arr[i] = rand() % n + 1;
   }
 }
-void BubbleSort(int* ar, int length, bool ascending){
+void BubbleSort(int* ar, int length, bool ascending, int& c){
   bool swapped = true;
   while(swapped){
     swapped = false;
     for(int i = 0; i < length; i++){
       if(ascending){
         if(ar[i] < ar[i+1]){
-        Swap(ar[i], ar[i+1]);
+        Swap(ar[i], ar[i+1], c);
         swapped = true;
         }
       }else{
         if(ar[i] > ar[i+1]){
-        Swap(ar[i], ar[i+1]);
+        Swap(ar[i], ar[i+1], c);
         swapped = true;
         }
       }
     }
   }
 }
-void SelectionSort(int* ar, int length, bool ascending){
-  for(int i = 0; i < length - 1; i++){
-    int min = ar[i];
-    for(int j = i + 1; i <  length; i++){
+void SelectionSort(int* ar, int length, bool ascending, int& c){
+  for(int i = 0; i <= length - 2; i++){
+    int min = i;
+    for(int j = i + 1; j <= length - 1; j++){
       if(ascending){
         if(ar[j] > ar[min]){
           min = j;
@@ -46,22 +47,21 @@ void SelectionSort(int* ar, int length, bool ascending){
           min = j;
         }
       }
-
     }
-    Swap(ar[i], ar[min]);
+    Swap(ar[i], ar[min], c);
   }
 }
-void InsertionSort(int* ar, int length, bool ascending){
+void InsertionSort(int* ar, int length, bool ascending, int& c){
   for(int i = 1; i < length; i++){
     int j = i;
     if(ascending){
       while(j > 0 && ar[j] > ar[j-1]){
-        Swap(ar[j], ar[j-1]);
+        Swap(ar[j], ar[j-1],c);
         j = j - 1;
       }
     }else{
       while(j > 0 && ar[j] < ar[j-1]){
-        Swap(ar[j], ar[j-1]);
+        Swap(ar[j], ar[j-1], c);
         j = j - 1;
       }
     }
@@ -77,7 +77,7 @@ bool isSorted(int* arr, int length, bool ascending){
   for(int i = 0; i < length; i++){
     if(ascending){
       if(arr[i] > arr[i + 1]){
-        return false
+        return false;
       }
     }else{
       if(arr[i] < arr[i+1]){
@@ -88,14 +88,24 @@ bool isSorted(int* arr, int length, bool ascending){
   return true;
 }
 int main(){
-  int length = 100;
+  int length = 100, count = 0;
   int a[length];
-  for(int i = 0;  i < length; i++){
-    a[i] = length;
-    length--;
-  }
+  fillArray(a, length);
   PrintArray(a, length);
-  BubbleSort(a, length);
+  // The "auto" type determines the correct type at compile-time
+  // -Dr.Anderson
+  auto start = std::chrono::system_clock::now();
+  InsertionSort(a, length, false, count); // replace with your sorting alorithm
+  auto end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end-start;
+  std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+  std::cout << "finished at " << std::ctime(&end_time)
+  << "elapsed time: " << elapsed_seconds.count() << "s\n";
   PrintArray(a, length);
+  std::cout << "Swap: " << count << std::endl;
+
+
+
+
 
 }
