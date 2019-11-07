7f3288832cb2d4d31b088b050204ce1958204d13
Blaine-Mason <bmason3@gulls.salisbury.edu>
2019-11-06 23:22:17 -0500

    Minor Progress

---
+++ Lab-09/mystack.h
@@ -0,0 +1,13 @@
+#ifndef MYSTACK_H
+#define MYSTACK_H
+
+
+template <class T>
+class MyStack{
+  private:
+    struct Node{
+      T data;
+      Node* next;
+    };
+/* Fill in stack operations: push, pop, peek */
+}
